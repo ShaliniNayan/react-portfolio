6e856f6a3037ba3f770cbecfa0551bd36cec9e78
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-12-04 10:46:56 +0530

    Update Home main with background img

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import HomeMain from '../components/HomeMain';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <HomeMain />
     </div>
   );
 };
