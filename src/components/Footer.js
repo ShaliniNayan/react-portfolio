c3a430f4362a59797e7c91ab7461a1f9ee0061fd
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-12-07 00:07:25 +0530

    Update the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,34 @@
 import './FooterStyles.css';
 
 import React from 'react';
+import { FaHome, FaMailBulk } from 'react-icons/fa';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='footer'>
+      <div className='footer-container'>
+        <div className='left'>
+          <div className='location'>
+            <FaHome size={20} style={{ color: '#fff', marginRight: '2rem' }} />
+            <div>
+              <p>Indore</p>
+              <p>India</p>
+            </div>
+          </div>
+          <div className='email'>
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: '#fff', marginRight: '2rem' }}
+              />
+              shalininayan@outlook.com
+            </h4>
+          </div>
+        </div>
+        <div className='right'></div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import HomeMain from '../components/HomeMain';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HomeMain />
+      <Footer />
     </div>
   );
 };
