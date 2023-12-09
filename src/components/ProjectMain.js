341fc68074839679a6744e8b59553188f48ecb18
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-12-09 22:38:20 +0530

    Create Project Page

---
+++ src/components/ProjectMain.js
@@ -0,0 +1,18 @@
+import './ProjectMainStyles.css';
+
+import React, { Component } from 'react';
+
+class ProjectMain extends Component {
+  render() {
+    return (
+      <div className='hero-img'>
+        <div className='heading'>
+          <h1>{this.props.heading}</h1>
+          <p>{this.props.text}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProjectMain;
