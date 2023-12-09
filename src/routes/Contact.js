a8ff848bfd4febd356adbc5fa32be7abd176b5c8
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-12-09 22:38:37 +0530

    Update the project page

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import ProjectMain from '../components/ProjectMain';
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
+      <ProjectMain
+        heading='CONTACT ME'
+        text='If you have an application you are interested in developing, a feature that you need built or a project that needs coding. I would love to help with it'
+      />
       <Footer />
     </div>
   );
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import ProjectMain from '../components/ProjectMain';
 
 const Project = () => {
-  return <div>
-    <Navbar />
-    <Footer />
-  </div>;
+  return (
+    <div>
+      <Navbar />
+      <ProjectMain heading='PROJECTS' text='Some of my most recent works' />
+      <Footer />
+    </div>
+  );
 };
 
 export default Project;
