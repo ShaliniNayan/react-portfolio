80d8d5bc032e7198416061adcb58586e5e9ddeaa
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-12-10 15:22:38 +0530

    Add & update image in project

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,7 +3,14 @@ import './ProjectCardStyles.css';
 import React from 'react';
 
 const ProjectCard = () => {
-  return <div>ProjectCard</div>;
+  return (
+    <div className='work-container'>
+      <h1 className='project-heading'>Projects</h1>
+      <div className='project-container'>
+        <div className='project-card'></div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectCard;
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import ProjectMain from '../components/ProjectMain';
+import ProjectCard from '../components/ProjectCard';
 
 const Project = () => {
   return (
     <div>
       <Navbar />
       <ProjectMain heading='PROJECTS' text='Some of my most recent works' />
+      <ProjectCard />
       <Footer />
     </div>
   );
