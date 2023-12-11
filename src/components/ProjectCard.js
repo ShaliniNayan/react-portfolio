bbcc0b600534570bce8a449fcd2377949d532fec
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-12-11 22:02:52 +0530

    Update project page with image

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,36 @@
 import './ProjectCardStyles.css';
 
 import React from 'react';
+import leader from '../images/leader.png';
+import { NavLink } from 'react-router-dom';
 
 const ProjectCard = () => {
   return (
-    <div className='work-container'>
-      <h1 className='project-heading'>Projects</h1>
-      <div className='project-container'>
-        <div className='project-card'></div>
-      </div>
-    </div>
+    <div className='project-card'>
+          <img src={leader} alt='Leader-board' />
+          <h2 className='project-title'>Leader-Board</h2>
+          <div className='project-details'>
+            <p>
+              This is a JavaScript project for a Leaderboard list app, which
+              uses modern web development tools and techniques such as webpack
+              and ES6 modules.
+            </p>
+            <div className='project-btns'>
+              <NavLink
+                to='https://github.com/ShaliniNayan/Leaderboard'
+                className='btn'
+              >
+                View
+              </NavLink>
+              <NavLink
+                to='https://shalininayan.github.io/Leaderboard/'
+                className='btn'
+              >
+                Source
+              </NavLink>
+            </div>
+          </div>
+        </div>
   );
 };
 
---
+++ src/components/Work.js
@@ -0,0 +1,18 @@
+import './ProjectCardStyles.css';
+
+import React from 'react';
+import leader from '../images/leader.png';
+import { NavLink } from 'react-router-dom';
+
+const Work = () => {
+  return (
+    <div className='work-container'>
+      <h1 className='project-heading'>Projects</h1>
+      <div className='project-container'>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Work;
