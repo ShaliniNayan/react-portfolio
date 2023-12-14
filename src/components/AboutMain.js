3a5c14abe2e64089c6c5d45b5017fc0e49ea79b1
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-12-14 20:09:12 +0530

    Update the image of language & framework

--- src/components/AboutMain.js
+++ src/components/AboutMain.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import './AboutMainStyles.css';
+import HtmlCssJS from '../images/js-html-css.png';
 
 import React from 'react';
 
@@ -9,13 +10,14 @@ const AboutMain = () => {
       <div className='left'>
         <h1>Let me Introduce myself</h1>
         <p>
-          I'am Shalini Nayan innovative Full-Stack Developer proficient in Ruby on Rails, React,
-          and Material-UI, with a knack for crafting seamless APIs. Elevated
-          user experience by implementing Jest for rigorous testing, ensuring
-          top-notch reliability and functionality. Excels in delivering
-          cutting-edge solutions that blend performance and aesthetics to create
-          remarkable digital experiences.Let's connect to discuss how we can
-          collaborate and take your projects to the next level.
+          I'am Shalini Nayan innovative Full-Stack Developer proficient in Ruby
+          on Rails, React, and Material-UI, with a knack for crafting seamless
+          APIs. Elevated user experience by implementing Jest for rigorous
+          testing, ensuring top-notch reliability and functionality. Excels in
+          delivering cutting-edge solutions that blend performance and
+          aesthetics to create remarkable digital experiences.Let's connect to
+          discuss how we can collaborate and take your projects to the next
+          level.
         </p>
         <Link to='/contact'>
           <button className='btn'>Contact</button>
@@ -24,7 +26,11 @@ const AboutMain = () => {
 
       <div className='right'>
         <h1>Languages & Frameworks</h1>
-        <div className='img-container'></div>
+        <div className='img-container'>
+          <div className='img-stack top'>
+            <img src={HtmlCssJS} />
+          </div>
+        </div>
       </div>
     </div>
   );
