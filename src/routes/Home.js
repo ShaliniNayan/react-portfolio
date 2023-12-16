c1e018ee4136261bef78f273c9b3f5bbc0204afe
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-12-16 21:49:51 +0530

    Add project in home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import HomeMain from '../components/HomeMain';
 import Footer from '../components/Footer';
+import Work from '../components/Work'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HomeMain />
+      <Work />
       <Footer />
     </div>
   );
